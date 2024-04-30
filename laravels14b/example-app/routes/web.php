c861490f5f70c35ed4b2601e8d872f79fc31602d
rhmdfrds1212 <163318781+rhmdfrds1212@users.noreply.github.com>
2024-04-30 16:54:26 +0700

    instal laravel

--- laravels14b/example-app/routes/web.php
+++ laravels14b/example-app/routes/web.php
@@ -5,3 +5,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
+Route::get('about', function () {
+    return "Halaman About";
+});
+
+Route::get('profil', function(){
+    return view('profil');
+});
