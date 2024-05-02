b820c532b72b33e2b491bc8267f148b3798b47d4
rhmdfrds1212 <163318781+rhmdfrds1212@users.noreply.github.com>
2024-05-02 14:56:14 +0700

    UPDATE

--- laravels14b/example-app/routes/web.php
+++ laravels14b/example-app/routes/web.php
@@ -13,3 +13,22 @@ Route::get('about', function () {
 Route::get('profil', function(){
     return view('profil');
 });
+
+// Route dengan parameter
+Route::get('welcome/{salam}', function ($salam){
+    // return 'Selamat '. $salam;
+    return view('salam')->with('viewsalam', $salam);
+});
+
+// Route tanpa parameter listdata
+// terdapat array $list
+Route::get('listdata', function (){
+    $list = ["Sistem Informasi", "Informatika", "Manajemen"];
+    $listmhs = [
+        ["npm" => 001, "nama" => "Gaek"],
+        ["npm" => 002, "nama" => "Gilang"]
+    ];
+    return view('listprodi')
+        ->with('viewlist', $list)
+        ->with('viewmhs', $listmhs);    
+});
